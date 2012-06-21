70246c11ac7047a004bf79b2c9c74795e146ac95
Johnny Karhinen <johnny@nurrd.se>
2012-06-21 12:07:32 +0200

    And that's about it...

---
+++ app.py
@@ -0,0 +1,19 @@
+from bottle import route, run, request
+from pygeoip import GeoIP
+import os
+
+geoip_db = GeoIP('GeoLiteCity.dat')
+
+@route('/')
+def index():
+    return 'try /locate/(ip) or /locate/me'
+
+@route('/locate/me')
+def locateme():
+    return locate(request['REMOTE_ADDR'])
+
+@route('/locate/:ip')
+def locate(ip):
+    return geoip_db.record_by_addr(ip)
+
+run(server='gevent', port=os.environ.get('PORT', 8080))
